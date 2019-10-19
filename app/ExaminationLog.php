8e766dc1df0eae5d8e4499de9fc327c1283499be
sonphfs <sonph@kaopiz.com>
2019-10-19 17:32:31 +0700

    add column to user table

--- app/ExaminationLog.php
+++ app/ExaminationLog.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ExaminationLog extends Model
 {
     protected $table = 'examination_logs';
+
+    public function questions()
+    {
+        return $this->belongsToMany(Question::class, 'question_logs');
+    }
 }
