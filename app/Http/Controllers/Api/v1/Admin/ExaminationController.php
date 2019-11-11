e3221a42ddc8f9a45d7acdb52f4374f5a6d7875c
sonphfs <sonph@kaopiz.com>
2019-11-11 17:12:02 +0700

    create examination and upload file to s3

--- app/Http/Controllers/Api/v1/Admin/ExaminationController.php
+++ app/Http/Controllers/Api/v1/Admin/ExaminationController.php
@@ -10,6 +10,8 @@ use App\ScoreConversion;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Exception;
+use Illuminate\Support\Facades\Storage;
 
 class ExaminationController extends Controller
 {
@@ -19,9 +21,27 @@ class ExaminationController extends Controller
         return $this->response(Examination::all());
     }
 
-    public function create()
+    public function create(Request $request)
     {
-
+        $exam = new Examination();
+        $requestData = $request->all();
+        if ($request->file('audio')) {
+            $file = $request->file('audio');
+            $name = time() . $file->getClientOriginalName();
+            $filePath = 'images/examinations/' . $name;
+            Storage::disk('s3')->put($filePath, file_get_contents($file));
+            $exam->audio = $filePath;
+        }
+        try {
+            $exam->title = $requestData['title'];
+            $exam->description = $requestData['description'];
+            $exam->type = $requestData['type'];
+            $exam->code = $this->_generateRandomString();
+            $exam->save();
+            return $this->response($exam);
+        }catch (Exception $e) {
+            $this->response('Create new examination has failed!');
+        }
     }
 
     public function show($id)
